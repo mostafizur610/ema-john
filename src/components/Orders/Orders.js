e28b11a002dc0202ac745d73b23625c07bba11d4
mostafizur610 <mostafizurrahman273@gmail.com>
2022-10-10 18:47:19 +0600

    shop router path solved

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
+    const products = useLoaderData();
     return (
         <div>
-            <h2>This is orders</h2>
+            <h2>This is orders: {products.length}</h2>
         </div>
     );
 };
